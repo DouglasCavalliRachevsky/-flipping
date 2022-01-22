81b4797375993f75865117495184b4c31b72ffe1
Douglas <douglas.rachevsky@aquiris.com.br>
2022-01-22 18:10:30 -0300

    level manager created

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField] private LevelRules levelRules;
+    [SerializeField] [Range(4, 16)] private int startingNumberOfPieces = 4;
+    private List<GameObject> piecesGameObjects = new List<GameObject>();
+    private List<Vector3Int> startingGameBoard = new List<Vector3Int>();
+
+    private void Awake()
+    {
+        InitializeNewLevel();
+    }
+
+#if UNITY_EDITOR
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            InitializeNewLevel();
+        }
+    }
+#endif
+
+    public void InitializeNewLevel()
+    {
+        if (piecesGameObjects.Count > 0)
+        {
+            for (int i = piecesGameObjects.Count - 1; i >= 0; i--)
+            {
+                Destroy(piecesGameObjects[i]);
+            }
+        }
+
+        piecesGameObjects = new List<GameObject>();
+        startingGameBoard = LevelGenerator.GenerateANewLevel(levelRules, startingNumberOfPieces);
+
+        foreach (var tile in startingGameBoard)
+        {
+            GameObject pieceGameObject = Instantiate(levelRules.PieceList[tile.z],
+                new Vector3(tile.x * levelRules.TileSize, 0, tile.y * levelRules.TileSize),
+                Quaternion.identity);
+            piecesGameObjects.Add(pieceGameObject);
+        }
+    }
+}
